463ea16cc2eaebf10202aa1279ebc603d6d8f8c0
gongzhenmu <gongzhenmu@users.noreply.github.com>
2020-01-30 15:42:25 -0500

    folder structure
    add component

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html'
+
+})
+export class PostCreateComponent {
+    newPost = ' ';
+    onAddPost() {
+      this.newPost = 'This is it';
+    }
+
+}

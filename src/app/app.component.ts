6cb769268aa8c90cb97a7f211a9a52931380d098
charlotte <charlottedyt@gmail.com>
2020-02-11 00:07:33 -0500

    add bootstrap and jquery, change css files to scss
    bootstrap and jquery are enabled so that the app is able to use on mobiles.
    change css files to scss could make the change theme funtion easier to use

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Post } from './posts/post.model';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 

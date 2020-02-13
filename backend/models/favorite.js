8cb94a6e33264c36e9864b9f75974c655749f9d1
Tao Li <33759063+li2918@users.noreply.github.com>
2020-02-13 09:55:31 -0500

    2/13/2020
    Created favorite.js use to record post liked, liked by and number of liked.
    Not finished yet

---
+++ backend/models/favorite.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const favorite = mongoose.Schema(
+    {
+        favByUserName: {type: String},
+        favUserName: {type: String},
+        numOfLike: {type: Int16Array}
+    }
+);
+
+module.exports = mongoose.model(favorite,favorite,favorite);

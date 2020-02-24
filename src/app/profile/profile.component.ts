a8e48576461c5392257f9e19bfe35b551c9ff895
Charlotte Du <33757670+charlottedyyt@users.noreply.github.com>
2020-02-24 12:16:28 -0500

    Update profile.component.ts

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Profile } from './profile.model';
+import { ProfileService } from './profile.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  profile: Profile[] = [];
+  private profileSub: Subscription;
 
+  constructor(public profileService: ProfileService) {}
+ 
   ngOnInit() {
+    this.profileService.getProfile();
+    this.profileSub = this.profileService.getProfileUpdateListener()
+      .subscribe((profile: Profile[]) => {
+        this.profile = profile;
+      });
+  }
+  ngOnDestroy() {
+    this.profileSub.unsubscribe();
   }
 
 }

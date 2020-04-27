4ebcfc4eddc2942c7c6efc11ce38cd2a1b8a848c
Tao Li <33759063+li2918@users.noreply.github.com>
2020-04-26 22:28:26 -0400

    4/26/202
    1. Sort in ascending added for future use

--- backend/routes/sortTemplet.js
+++ backend/routes/sortTemplet.js
@@ -1,6 +1,6 @@
 
 //Sort in ascending
-var MongoClient = require('mongodb').MongoClient;
+/*var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, function(err, db) {
@@ -12,6 +12,17 @@ MongoClient.connect(url, function(err, db) {
     console.log(result);
     db.close();
   });
-});
+});*/
 
 ////Sort in descending order
+
+/*MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  var mysort = { name: -1 };
+  dbo.collection("customers").find().sort(mysort).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+});*/

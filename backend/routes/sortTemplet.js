887ef246f4435defcfffc1a6492d10d08750cf04
Tao Li <33759063+li2918@users.noreply.github.com>
2020-04-26 22:25:39 -0400

    4/26/2020
    1. Add templete for ascending order sort

---
+++ backend/routes/sortTemplet.js
@@ -0,0 +1,17 @@
+
+//Sort in ascending
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  var mysort = { name: 1 };
+  dbo.collection("customers").find().sort(mysort).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+});
+
+////Sort in descending order

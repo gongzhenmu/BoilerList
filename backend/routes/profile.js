9891dc2b08bd344454943a98b718257ad0350dea
charlotte <charlottedyt@gmail.com>
2020-04-29 00:18:04 -0400

    rating bug fix

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -207,7 +207,7 @@ router.post('/rate',checkAuth,(req,res,next)=>{
   const userData=req.body;
   console.log("rate"+userData.rate);
   userM.findOneAndUpdate({username:userData.username},{$inc : {ratingCount:1, ratings:userData.rate}}).then(updatePost =>{
-    const review = new review({
+    const review = new reviewM({
       sellername:userData.username,
       rate:userData.rate,
       content:userData.content
